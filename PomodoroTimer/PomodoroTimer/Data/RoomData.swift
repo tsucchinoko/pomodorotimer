012c31521f64d6dfbfc9bae96c063a277c98bd0d
tsuchinoko <ccya2211@icloud.com>
2021-08-09 19:24:53 +0900

    bgm名を修正

--- PomodoroTimer/PomodoroTimer/Data/RoomData.swift
+++ PomodoroTimer/PomodoroTimer/Data/RoomData.swift
@@ -13,9 +13,9 @@ import SwiftUI
 //var bgColor: [Color]
 // MARK: - ROOM MODEL DATA
 let roomData: [Room] = [
-    Room(roomName: "森の部屋", bgm: "森", gradientColors: [Color("ColorForestLight"), Color("ColorForestDark")]),
-    Room(roomName: "海の部屋", bgm: "海", gradientColors: [Color("ColorSeaLight"), Color("ColorSeaDark")]),
-    Room(roomName: "ロウリュウの部屋", bgm: "火", gradientColors: [Color("ColorFireLight"), Color("ColorFireDark")]),
+    Room(roomName: "森の部屋", bgm: "forest", gradientColors: [Color("ColorForestLight"), Color("ColorForestDark")]),
+    Room(roomName: "海の部屋", bgm: "sea", gradientColors: [Color("ColorSeaLight"), Color("ColorSeaDark")]),
+    Room(roomName: "ロウリュウの部屋", bgm: "fire", gradientColors: [Color("ColorFireLight"), Color("ColorFireDark")]),
 //    TODO 追加
 //    Room(roomName: "星の部屋", bgm: "星", gradientColors: [Color("ColorCosmoLight"), Color("ColorCosmoDark")]),
 //    Room(roomName: "カフェの部屋", bgm: "コーヒー", gradientColors: [Color("ColorCoffeeLight"), Color("ColorCoffeeDark")])
